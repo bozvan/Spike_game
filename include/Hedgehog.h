f89ddff7d8a69e9de209562a61b34013b660d320
bozvan <ivan.bozvanoff@yandex.ru>
2026-03-10 13:36:04 +0300

    feat(Hedgehog): add jump function with different directions

--- include/Hedgehog.h
+++ include/Hedgehog.h
@@ -3,16 +3,32 @@
 
 #include <SFML/Graphics.hpp>
 
+enum Direction
+{
+    LEFT,
+    RIGHT,
+    DEFAULT
+};
+
 class Hedgehog {
 private:
     sf::RectangleShape shape;
     sf::Vector2f movement;
     bool isPressed;
+    bool jumping;
+
+    float jump_force;
+    float gravity;
+    sf::Vector2f speed;
+    float speedRun;
 
 public:
     Hedgehog(sf::Vector2f position, sf::Vector2f size, sf::Color color);
     void handleEvent(const sf::Event& event, const sf::RenderWindow& window);
+    void stop_falling(float y);
+    void set_position(const sf::Vector2f& pos);
     void draw(sf::RenderWindow& window);
+    void jump(Direction direction);
     void update(sf::Time &deltaTime);
     bool wasClicked() const;
 };
--- src/Hedgehog.cpp
+++ src/Hedgehog.cpp
@@ -1,8 +1,10 @@
 #include "Hedgehog.h"
 #include <iostream>
 
+
 Hedgehog::Hedgehog(sf::Vector2f position, sf::Vector2f size, sf::Color color)
-    : isPressed(false)
+    : isPressed(false),
+      jumping(0), jump_force(250), gravity(1000), speed(0, 0), speedRun(200.f)
 {
     shape.setPosition(position);
     shape.setSize(size);
@@ -33,25 +35,84 @@ void Hedgehog::draw(sf::RenderWindow& window)
     window.draw(shape);
 }
 
+void Hedgehog::stop_falling(float y)
+{
+    if (jumping)
+    {
+        jumping = false;
+        speed.y = 0;
+        shape.setPosition({shape.getPosition().x, y});
+    }
+}
+
+void Hedgehog::jump(Direction direction=Direction::DEFAULT)
+{
+    bool falling = false;
+    if (!jumping)
+    {
+        speed.x = 0;
+        if (direction == Direction::LEFT)
+        {
+            speed.x -= speedRun;
+        }
+        else if (direction == Direction::RIGHT)
+        {
+            speed.x += speedRun;
+        }
+        if (!falling)
+            speed.y = -jump_force;
+        jumping = true;
+    }
+}
+
+void Hedgehog::set_position(const sf::Vector2f& pos)
+{
+    shape.setPosition(pos);
+}
+
 void Hedgehog::update(sf::Time &deltaTime)
 {
     movement.x = 0.f;
     movement.y = 0.f;
-    const float speed = 200.f;
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space) &&
+        sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+    {
+        jump(Direction::LEFT);
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space) &&
+          sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+    {
+        jump(Direction::RIGHT);
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space))
+        jump();
+
+    if (shape.getPosition().y > 400)
+        stop_falling(400);
+
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
-        movement.y -= speed;
+        movement.y -= speedRun;
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
-        movement.x -= speed;
+        movement.x -= speedRun;
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
-        movement.x += speed;
+        movement.x += speedRun;
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
-        movement.y += speed;
+        movement.y += speedRun;
 
     float length = std::sqrt(movement.x * movement.x + movement.y * movement.y);
     if (length != 0)
         movement /= length;
 
-    shape.move(movement * speed * deltaTime.asSeconds());
+    if (jumping)
+    {
+        speed.y += gravity * deltaTime.asSeconds();
+        shape.move(speed * deltaTime.asSeconds());
+    }
+    else
+    {
+        shape.move(movement * speedRun * deltaTime.asSeconds());
+    }
 }
 
 bool Hedgehog::wasClicked() const
